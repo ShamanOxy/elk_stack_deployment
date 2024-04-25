f7e3ee5a50d3c9f91ad19f18d3ac96647c7b8591
ShamanOxy_ <106046849+ShamanOxy@users.noreply.github.com>
2024-04-24 23:40:00 -0400

    Update apt-update.sh

--- Linux/apt-update.sh
+++ Linux/apt-update.sh
@@ -7,7 +7,6 @@ apt update -y
 apt upgrade -y
 
 # Install new packages, and uninstall any old packages that
-# must be removed to install them
 apt full-upgrade -y
 
 # Remove unused packages and their associated configuration files
